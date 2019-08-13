6e1c7cafe79f4f921bd8d689f48c0873aa9e7f1b
YuefengTang <17125161@bjtu.edu.cn>
2019-08-13 11:07:13 +0800

    添加test工程到本地git

---
+++ idea-git-test/src/com/moon/git/Git.java
@@ -0,0 +1,10 @@
+package com.moon.git;
+
+public class Git {
+
+    public static void main(String[] args) {
+        System.out.println("git idea project");
+        System.out.println("git idea project");
+        System.out.println("git idea project");
+    }
+}
